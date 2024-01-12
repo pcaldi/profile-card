1a1fa79939b3b772d880027df1fb6062b1118995
pcaldi <pcaldi@gmail.com>
2024-01-11 23:43:24 -0300

    mudar a foto quando muda o theme

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -2,17 +2,24 @@ import { Image, Text, View } from "react-native"
 
 import { Techs } from "@/components/Techs"
 import { stylesheet } from "./styles"
-import { useStyles } from "react-native-unistyles"
+import { useStyles, UnistylesRuntime } from "react-native-unistyles"
 
 export function ProfileCard() {
   const { styles } = useStyles(stylesheet)
   return (
     <View style={styles.container}>
-      <Image
-        source={require("@/assets/images/paulo.png")}
-        style={styles.avatar}
-      />
-
+      {UnistylesRuntime.themeName === "dark" ? (
+        <Image
+          source={require("@/assets/images/paulo.png")}
+          style={styles.avatar}
+        />
+      ) : (
+        <Image
+          source={require("@/assets/images/paulo_02.png")}
+          style={styles.avatar}
+        />
+      )
+      }
       <Image
         source={require("@/assets/images/react_logo.png")}
         style={styles.react_logo}
